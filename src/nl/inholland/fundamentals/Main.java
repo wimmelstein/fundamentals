a6e5099098be7b2fd18df655dd953e77420b0ad8
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-01 12:27:34 +0200

    Week 1 assignment 1

--- src/nl/inholland/fundamentals/Main.java
+++ src/nl/inholland/fundamentals/Main.java
@@ -1,8 +1,33 @@
 package nl.inholland.fundamentals;
 
+import java.util.Scanner;
+
 public class Main {
 
+    /*
+    Creating scanner as a class variable
+     */
+
+    Scanner scanner = null;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Creating a start method. But then we have to instantiate this class...
+        Main main = new Main();
+        main.start();
+    }
+
+    private void start() {
+        // Initialize scanner and read in a string
+        scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        // Read in an int
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println(String.format("You are %s, and you are %d years old", name, age));
+
+        // This is System I/O. It's good practice to close the scanner...
+        scanner.close();
     }
 }
