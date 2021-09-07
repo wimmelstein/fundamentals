493bd41b433afb9bf9f84c83aed74fa8e3d0d79b
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-07 11:08:24 +0200

    Finished assignment 2

--- src/nl/inholland/fundamentals/App.java
+++ src/nl/inholland/fundamentals/App.java
@@ -2,7 +2,7 @@ package nl.inholland.fundamentals;
 
 import java.util.Scanner;
 
-public class Main {
+public class App {
 
     String course;
     int numberOfStudents;
@@ -11,12 +11,12 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Main main = new Main();
-        main.readBaseData();
-        main.readStudents(main.numberOfStudents);
-        main.readGrades(main.numberOfStudents);
-        main.printResults();
-        main.scanner.close();
+        App app = new App();
+        app.readBaseData();
+        app.readStudents(app.numberOfStudents);
+        app.readGrades(app.numberOfStudents);
+        app.printResults();
+        app.scanner.close();
     }
 
     private void printResults() {
@@ -28,18 +28,17 @@ public class Main {
             sum += grades[i];
         }
         double avgResult = sum/numberOfStudents;
-        System.out.printf("Average score: %d%n", numberOfStudents);
         for (int i = 0; i < numberOfStudents; i++) {
             if (grades[i] > maxResult) {
                 studentWithHighestScore = i;
                 maxResult = grades[i];
             }
         }
-        System.out.printf("Student %s has highest score of %d", students[studentWithHighestScore], maxResult);
-        System.out.println();
+        System.out.printf("Average grade: %.1f%n", avgResult);
+        System.out.printf("Student %s has highest score of %d%n%n", students[studentWithHighestScore], maxResult);
 
         for (int i = 0; i < numberOfStudents; i++) {
-            System.out.printf("Grade for student %s (course %s): %d", students[i], course, grades[i] );
+            System.out.printf("Grade for student %s (course %s): %d%n", students[i], course, grades[i] );
         }
     }
 
@@ -47,17 +46,19 @@ public class Main {
     private void readGrades(int numberOfStudents) {
         grades = new int[numberOfStudents];
         for (int i = 0; i < numberOfStudents; i++) {
-            System.out.print("Grade for " + students[i]);
+            System.out.printf("Enter grade for %s: ", students[i]);
             grades[i] = scanner.nextInt();
         }
+        System.out.println();
     }
 
     private void readStudents(int numberOfStudents) {
         students = new String[numberOfStudents];
         for (int i = 0; i < numberOfStudents; i++) {
-            System.out.print("Student name: ");
+            System.out.printf("Enter name of student %d: ", i+1);
             students[i] = scanner.nextLine();
         }
+        System.out.println();
     }
 
     private void readBaseData() {
@@ -65,5 +66,6 @@ public class Main {
         course = scanner.nextLine();
         System.out.print("Number of students: ");
         numberOfStudents = scanner.nextInt();
+        scanner.nextLine();
     }
 }
