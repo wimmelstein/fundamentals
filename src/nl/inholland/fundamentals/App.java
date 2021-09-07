b00f8ca28511d6d9cc433206ae03b7281b17a437
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-07 15:53:49 +0200

    Rewrote the adding to array of people

--- src/nl/inholland/fundamentals/App.java
+++ src/nl/inholland/fundamentals/App.java
@@ -1,8 +1,27 @@
 package nl.inholland.fundamentals;
 
+import nl.inholland.fundamentals.model.Person;
+import nl.inholland.fundamentals.model.Student;
+import nl.inholland.fundamentals.model.Teacher;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Student jack = new Student("Jack Monterrey", "jack.monterrey@student.inholland.nl", "IT2a");
+        Student lisa = new Student("Lisa Ventura", "lisa.ventura@student.inholland.nl", "IT2a");
+        Teacher jill = new Teacher("Jill Summers", "jill.summers@inholland.nl", 3500.00);
+        Teacher peter = new Teacher("Peter Prince", "peter.prince@inholland.nl", 2790.85);
+
+        Person[] people = new Person[]{jack, lisa, jill, peter};
+
+        for (Person person : people) {
+            if (person instanceof Teacher) {
+                double yearlySalary = ((Teacher) person).getSalary();
+                System.out.println(person + ", yearly salary=" + String.format("%.2f", yearlySalary * 12.00));
+            } else {
+                System.out.println(person);
+            }
+        }
+
     }
 }
