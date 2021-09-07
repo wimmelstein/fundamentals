910f1133a8365a5d2899f28af131a5d7728b01f1
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-07 15:53:49 +0200

    Rewrote the adding to array of people

--- src/nl/inholland/fundamentals/App.java
+++ src/nl/inholland/fundamentals/App.java
@@ -7,16 +7,12 @@ import nl.inholland.fundamentals.model.Teacher;
 public class App {
 
     public static void main(String[] args) {
-        Person[] people = new Person[4];
         Student jack = new Student("Jack Monterrey", "jack.monterrey@student.inholland.nl", "IT2a");
         Student lisa = new Student("Lisa Ventura", "lisa.ventura@student.inholland.nl", "IT2a");
         Teacher jill = new Teacher("Jill Summers", "jill.summers@inholland.nl", 3500.00);
         Teacher peter = new Teacher("Peter Prince", "peter.prince@inholland.nl", 2790.85);
 
-        people[0] = jack;
-        people[1] = lisa;
-        people[2] = jill;
-        people[3] = peter;
+        Person[] people = new Person[]{jack, lisa, jill, peter};
 
         for (Person person : people) {
             if (person instanceof Teacher) {
