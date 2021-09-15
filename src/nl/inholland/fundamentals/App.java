5b8323599b0612d784b98d13a3b76a8d97b3c2fb
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-15 15:46:59 +0200

    Underpaid the freelancer

--- src/nl/inholland/fundamentals/App.java
+++ src/nl/inholland/fundamentals/App.java
@@ -3,6 +3,27 @@ package nl.inholland.fundamentals;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        String placeholder = "I like Java";
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            placeholder += "I like Java";
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Adding to the string took  " + (end - start) + " ms");
+
+        long start2 = System.currentTimeMillis();
+        StringBuffer stringBuffer = new StringBuffer(placeholder);
+        for (int i = 0; i < 100000; i++) {
+            stringBuffer.append("I like Java");
+        }
+        long end2 = System.currentTimeMillis();
+        System.out.println("This took about " + (end2 - start2) + " ms");
+
+        long start3 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder(placeholder);
+        for (int i = 0; i < 100000; i++) {
+            stringBuilder.append("I like Java");
+        }
+        long end3 = System.currentTimeMillis();
+        System.out.println("This took about " + (end3 - start3) + " ms");    }
 }
