eaf4155713149cd6b10d82dcde4fa4c9f09bbc0a
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-15 10:16:43 +0200

    Refactored to its own method

--- src/nl/inholland/fundamentals/App.java
+++ src/nl/inholland/fundamentals/App.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) {
+        App app = new App();
         Employee employee = new Employee();
         employee.setGrossSalary(5000);
 
@@ -18,7 +19,11 @@ public class App {
 
         List<Payable> payables = List.of(freelancer,employee);
         for (Payable payable: payables) {
-            System.out.println(payable.getPayout());
+            app.displayPayment(payable);
         }
+    }
+
+    public void displayPayment(Payable payable) {
+        System.out.println(payable.getClass().getSimpleName() + ": " + payable.getPayout());
     }
 }
