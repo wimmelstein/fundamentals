5bd65c21c0aa44a53dbee976d66bfa8853791278
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-15 10:39:17 +0200

    Refactored to its own method

--- src/nl/inholland/fundamentals/App.java
+++ src/nl/inholland/fundamentals/App.java
@@ -1,8 +1,25 @@
 package nl.inholland.fundamentals;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<String> words = List.of("wing", "inconclusive", "drain", "uninterested", "arrive", "bulb", "friction", "deliver", "temper", "wiggly", "tested", "plug");
+        Map<Integer, List<String>> map = new HashMap<>();
+
+        for (String word : words) {
+            List<String> list = map.getOrDefault(word.length(), new ArrayList<>());
+            list.add(word);
+            map.put(word.length(), list);
+        }
+
+        for (int i : map.keySet()) {
+            System.out.println(i + ": "  + map.get(i));
+        }
     }
 }
